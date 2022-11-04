1fd905611c3d8adc4106bc158815bb66a2f1beb7
Tiago Lourinho <lourinho.tiago@gmail.com>
2022-11-04 15:27:45 -0300

    feat: intro project

---
+++ Examples/Intro/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("\nQual é a frase que todo desenvolvedor deve printar quando começa a estudar uma nova linguagem?");
+string? welcome = Console.ReadLine();
+Console.WriteLine($"\n{welcome}");
