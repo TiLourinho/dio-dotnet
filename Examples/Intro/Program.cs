0296d831db2e8622bb9cf6eaf594d95e4b2d81d6
Tiago Lourinho <lourinho.tiago@gmail.com>
2022-11-04 18:35:49 -0300

    Delete Examples directory

--- Examples/Intro/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿Console.WriteLine("\nQual é a frase que todo desenvolvedor deve printar quando começa a estudar uma nova linguagem?");
-string? welcome = Console.ReadLine();
-Console.WriteLine($"\n{welcome}");
