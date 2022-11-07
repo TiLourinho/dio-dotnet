317eb41514027fa895562b6f97e6e37ed9c2fd14
Tiago Lourinho <lourinho.tiago@gmail.com>
2022-11-07 20:08:29 -0300

    feat: example

---
+++ 01 - Fundamentos .NET/Bloco 04 - Conhecendo a Estrutura de um Programa C#/Seção 01 - Estrutura de um Programa e Método Principal/Exemplos/Company/Employee/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace Individual.Models
+{
+	public class Employee
+	{
+		public string? name;
+		public double grossSalary;
+		public double tax;
+
+		public double NetSalary()
+		{
+				double netSalary = grossSalary - tax;
+				return netSalary;
+		}
+
+		public void IncreaseSalary(double percentage)
+		{
+				double newSalary = grossSalary * ((percentage / 100) + 1);
+				grossSalary = newSalary;
+		}
+	}
+}
