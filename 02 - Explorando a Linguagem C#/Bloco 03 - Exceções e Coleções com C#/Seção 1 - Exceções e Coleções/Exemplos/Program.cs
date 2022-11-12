d931474cbb252e41a4cd3e0e2d94b2a65005784f
Tiago Lourinho <lourinho.tiago@gmail.com>
2022-11-12 12:20:44 -0300

    fix: uncommenting the code

--- 02 - Explorando a Linguagem C#/Bloco 03 - Exceções e Coleções com C#/Seção 1 - Exceções e Coleções/Exemplos/Program.cs
+++ 02 - Explorando a Linguagem C#/Bloco 03 - Exceções e Coleções com C#/Seção 1 - Exceções e Coleções/Exemplos/Program.cs
@@ -2,82 +2,82 @@
 
 /* Try / Catch */
 
-// try
-// {
-//     string[] rows = File.ReadAllLines("Files/readFile.txt");
+try
+{
+    string[] rows = File.ReadAllLines("Files/readFile.txt");
 
-//     foreach (string row in rows)
-//     {
-//         Console.WriteLine(row);
-//     }
-// }
-// catch(FileNotFoundException ex)
-// {
-//     Console.WriteLine($"While reading the file an error occurred! File not found! {ex.Message}");
-// }
-// catch(DirectoryNotFoundException ex)
-// {
-//     Console.WriteLine($"While reading the file an error occurred! Directory not found! {ex.Message}");
-// }
-// catch(Exception ex)
-// {
-//     Console.WriteLine($"A generic exception occurred! {ex.Message}");
-// }
-// finally
-// {
-//     // O finally sempre é executado! Com erros ou não
-//     Console.WriteLine($"Got here!");
-// }
+    foreach (string row in rows)
+    {
+        Console.WriteLine(row);
+    }
+}
+catch(FileNotFoundException ex)
+{
+    Console.WriteLine($"While reading the file an error occurred! File not found! {ex.Message}");
+}
+catch(DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"While reading the file an error occurred! Directory not found! {ex.Message}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"A generic exception occurred! {ex.Message}");
+}
+finally
+{
+    // O finally sempre é executado! Com erros ou não
+    Console.WriteLine($"Got here!");
+}
 
 /* Throw */
 
-// new ExceptionExample().Method1();
+new ExceptionExample().Method1();
 
 /* Filas */
 
-// Queue<int> intQueue = new();
+Queue<int> intQueue = new();
 
-// intQueue.Enqueue(2);
-// intQueue.Enqueue(4);
-// intQueue.Enqueue(6);
-// intQueue.Enqueue(8);
+intQueue.Enqueue(2);
+intQueue.Enqueue(4);
+intQueue.Enqueue(6);
+intQueue.Enqueue(8);
 
-// foreach (int item in intQueue)
-// {
-//     Console.WriteLine(item);
-// }
+foreach (int item in intQueue)
+{
+    Console.WriteLine(item);
+}
 
-// Console.WriteLine($"Removing element: {intQueue.Dequeue()}");
-// intQueue.Enqueue(10);
+Console.WriteLine($"Removing element: {intQueue.Dequeue()}");
+intQueue.Enqueue(10);
 
-// foreach (int item in intQueue)
-// {
-//     Console.WriteLine(item);
-// }
+foreach (int item in intQueue)
+{
+    Console.WriteLine(item);
+}
 
-// Console.Write(string.Join(", ", intQueue));
+Console.Write(string.Join(", ", intQueue));
 
 /* Pilhas */
 
-// Stack<int> intStack = new();
+Stack<int> intStack = new();
 
-// intStack.Push(4);
-// intStack.Push(6);
-// intStack.Push(8);
-// intStack.Push(10);
+intStack.Push(4);
+intStack.Push(6);
+intStack.Push(8);
+intStack.Push(10);
 
-// foreach (int item in intStack)
-// {
-//     Console.WriteLine(item);
-// }
+foreach (int item in intStack)
+{
+    Console.WriteLine(item);
+}
 
-// Console.WriteLine($"Removing element: {intStack.Pop()}");
-// intStack.Push(12);
+Console.WriteLine($"Removing element: {intStack.Pop()}");
+intStack.Push(12);
 
-// foreach (int item in intStack)
-// {
-//     Console.WriteLine(item);
-// }
+foreach (int item in intStack)
+{
+    Console.WriteLine(item);
+}
 
 /* Dicionários */
 
