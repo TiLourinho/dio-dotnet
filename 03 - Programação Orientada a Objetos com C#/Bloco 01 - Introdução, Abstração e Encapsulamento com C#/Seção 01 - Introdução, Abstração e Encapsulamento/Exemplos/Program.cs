cbc547fafa2c430d123d9456cc2ea13911d9aa90
Tiago Lourinho <lourinho.tiago@gmail.com>
2022-11-23 12:16:24 -0300

    refactor: comments

--- 03 - Programação Orientada a Objetos com C#/Bloco 01 - Introdução, Abstração e Encapsulamento com C#/Seção 01 - Introdução, Abstração e Encapsulamento/Exemplos/Program.cs
+++ 03 - Programação Orientada a Objetos com C#/Bloco 01 - Introdução, Abstração e Encapsulamento com C#/Seção 01 - Introdução, Abstração e Encapsulamento/Exemplos/Program.cs
@@ -2,21 +2,15 @@
 
 /* Classes e Objetos */
 
-/*
-    Uma classe é uma representação de algo do mundo real, com suas características e o objeto é sua implementação, a concretização de uma classe.
-*/
+// Uma classe é uma representação de algo do mundo real, com suas características e o objeto é sua implementação, a concretização de uma classe.
 
 /* Paradigmas de Programação */
 
-/*
-    Um paradigma é um conjunto de regras para solucionar um problema específico. Uma linguagem de programação implementa um ou mais paradigmas.
-*/
+// Um paradigma é um conjunto de regras para solucionar um problema específico. Uma linguagem de programação implementa um ou mais paradigmas.
 
 /* Abstração */
 
-/*
-    Abstrair um objeto do mundo real para um contexto específico, considerando apenas os atributos importantes.
-*/
+// Abstrair um objeto do mundo real para um contexto específico, considerando apenas os atributos importantes.
 
 Person p1 = new();
 p1.Name = "Giovanna";
@@ -26,9 +20,7 @@ p1.Introduce();
 
 /* Encapsulamento */
 
-/*
-    O encapsulamento serve para proteger uma classe e definir limites para alteração de suas propriedades. Oculta o seu comportamento e expõe somente o necessário.
-*/
+// O encapsulamento serve para proteger uma classe e definir limites para alteração de suas propriedades. Oculta o seu comportamento e expõe somente o necessário.
 
 CurrentAccount account1 = new(356999, 1000);
 
