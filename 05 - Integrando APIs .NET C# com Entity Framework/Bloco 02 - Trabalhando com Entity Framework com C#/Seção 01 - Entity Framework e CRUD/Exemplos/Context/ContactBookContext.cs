cade18f3db91c05d6315a353813a0d4979e2f551
Tiago Lourinho <lourinho.tiago@gmail.com>
2023-01-02 17:32:57 -0300

    feat: Contact Book CRUD

--- 05 - Integrando APIs .NET C# com Entity Framework/Bloco 02 - Trabalhando com Entity Framework com C#/Seção 01 - Entity Framework e CRUD/Exemplos/Context/ContactBookContext.cs
+++ 05 - Integrando APIs .NET C# com Entity Framework/Bloco 02 - Trabalhando com Entity Framework com C#/Seção 01 - Entity Framework e CRUD/Exemplos/Context/ContactBookContext.cs
@@ -9,10 +9,7 @@ namespace Exemplos.Context
 {
     public class ContactBookContext : DbContext
     {
-        public ContactBookContext(DbContextOptions<ContactBookContext> options) : base(options)
-        {
-
-        }
+        public ContactBookContext(DbContextOptions<ContactBookContext> options) : base(options) { }
 
         public DbSet<Contact> Contacts { get; set; }
     }
