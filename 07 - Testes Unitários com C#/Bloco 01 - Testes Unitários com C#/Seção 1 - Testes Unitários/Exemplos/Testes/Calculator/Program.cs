20ec7ccbdf78dbc18c18031968b4d2a76df17c12
Tiago Lourinho <lourinho.tiago@gmail.com>
2023-03-22 17:02:21 -0300

    chore: renamed folder

---
+++ 07 - Testes Unitários com C#/Bloco 01 - Testes Unitários com C#/Seção 1 - Testes Unitários/Exemplos/Testes/Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using Calculator.Services;
+
+CalculatorImp calculator = new();
+
+int num1 = 10;
+int num2 = 5;
+
+Console.WriteLine("Sum:");
+Console.WriteLine($"{num1} + {num2} = {calculator.Sum(num1, num2)}\n");
+Console.WriteLine("Subtraction:");
+Console.WriteLine($"{num1} - {num2} = {calculator.Subtraction(num1, num2)}\n");
+Console.WriteLine("Multiplication:");
+Console.WriteLine($"{num1} * {num2} = {calculator.Multiplication(num1, num2)}\n");
+Console.WriteLine("Division:");
+Console.WriteLine($"{num1} / {num2} = {calculator.Division(num1, num2)}");
