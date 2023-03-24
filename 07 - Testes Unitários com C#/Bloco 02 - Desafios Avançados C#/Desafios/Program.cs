a68c1445d372f4bf888bf5dd6344e2a77155d305
Tiago Lourinho <lourinho.tiago@gmail.com>
2023-03-24 01:28:51 -0300

    feat: HoHoHo challenge done

--- 07 - Testes Unitários com C#/Bloco 02 - Desafios Avançados C#/Desafios/Program.cs
+++ 07 - Testes Unitários com C#/Bloco 02 - Desafios Avançados C#/Desafios/Program.cs
@@ -18,6 +18,12 @@ static void ValidacaoDeNota()
     Se uma nota inválida for lida, deve ser impressa a mensagem "nota invalida".
     Quando duas notas válidas forem lidas, deve ser impressa a mensagem "media = " seguido do valor do cálculo.
     O valor deve ser apresentado com duas casas após o ponto decimal.
+
+    Exemplos de entrada:           Exemplos de saída:
+    -3.5                           nota invalida
+    3.5
+    11.0                           nota invalida
+    10.0                           media = 6.75
     */
 
     CultureInfo.CurrentCulture = new CultureInfo("en-US");
@@ -44,3 +50,42 @@ static void ValidacaoDeNota()
 }
 
 ValidacaoDeNota();
+
+static void HoHoHo()
+{
+    /*
+    Desafio 2
+
+    Papai Noel está brincando com seus duendes para entretê-los durante a véspera do Natal.
+    A brincadeira consiste nos elfos escreverem números em pedaços de papel e colocarem no gorro do Papai Noel.
+    Após todos terminarem de colocar os números Noel sorteia um papel e aquele número representa quantos "Ho" o Noel deve falar.
+
+    Seu trabalho é ajudar o Papai Noel montando um problema que mostre todos os "Ho" que ele deve falar dado o número sorteado.
+
+    Entrada:
+    A entrada é composta por um único inteiro N (0 < N ≤ 106) representando quantos "Ho" serão falados por Noel.
+
+    Saída:
+    A saída é composta por todos "Ho" que Papai Noel deve falar separados por um espaço.
+    Após o último "Ho" deve ser apresentado um "!" encerrando o programa.
+
+    Exemplos de entrada:           Exemplos de saída:
+    5                              Ho Ho Ho Ho Ho!
+    */
+
+    int numberOfHos = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 0; i < numberOfHos; i += 1)
+    {
+        if (i != numberOfHos - 1)
+        {
+            Console.WriteLine("Ho ");
+        }
+        else
+        {
+            Console.WriteLine("Ho!");
+        }
+    }
+}
+
+HoHoHo();
